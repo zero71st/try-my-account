2acbe19ddc59840553ac55fab839660ec051689f
kasem <kasem.w@dzcard.com>
2018-09-28 16:27:52 +0700

    add hero-section

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <section className="hero is-info is-small">
+          <div className="hero-body">
+            <h1 className="title">Flow Account</h1>
+          </div>
+      </section>
     );
   }
 }
